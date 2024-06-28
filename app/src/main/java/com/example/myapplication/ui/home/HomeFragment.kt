77184f6aa0e87154316c5213b265559dcb6c385c
seoyeong <carsk0607@gmail.com>
2024-06-28 15:12:03 +0900

    bottom navigation templates 활용

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentHomeBinding
 import com.google.gson.Gson
 
@@ -24,10 +27,7 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         loadContactsFromJson()
-
-        // 이름과 연락처를 TextView에 표시
-        binding.nameTextView.text = contacts[0].name
-        binding.phoneTextView.text = contacts[0].phoneNumber
+        bindContacts()
 
         return root
     }
@@ -39,11 +39,35 @@ class HomeFragment : Fragment() {
 
     private fun loadContactsFromJson() {
         val jsonString = context?.assets?.open("contacts.json")?.bufferedReader().use { it?.readText() }
-        contacts = Gson().fromJson(jsonString, Array<Contact>::class.java).toList()
+        Log.d("HomeFragment", "jsonString: $jsonString")
+
+        val contactsData = Gson().fromJson(jsonString, ContactsData::class.java)
+        contacts = contactsData.data
+
+        Log.d("HomeFragment", "contactsString: $contacts")
+    }
+
+    private fun bindContacts() {
+        binding.contactsLayout.removeAllViews() // 기존 뷰들을 제거
+
+        contacts.forEach { contact ->
+            // 새로운 뷰를 inflate하여 추가
+            val contactView = LayoutInflater.from(requireContext())
+                .inflate(R.layout.item_contact, binding.contactsLayout, false)
+
+            contactView.findViewById<TextView>(R.id.nameTextView).text = contact.name
+            contactView.findViewById<TextView>(R.id.phoneTextView).text = contact.phoneNumber
+
+            binding.contactsLayout.addView(contactView)
+        }
     }
 }
 
 data class Contact(
     val name: String,
     val phoneNumber: String
+)
+
+data class ContactsData(
+    val data: List<Contact>
 )
